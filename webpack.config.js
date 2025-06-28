c1fabfc89d81f47d70cebd6f2f5022ed787bda89
shocknet-justin <justin@shock.network>
2025-06-27 21:04:58 -0400

    css in head

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,62 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: {
-        utils: './src/utils.ts',
-        main: './src/index.ts',
-        debit: './src/debit.ts',
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            chunks: ['main', 'utils'],
-            favicon: './src/favicon.png',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/debit.html',
-            filename: 'debit.html',
-            chunks: ['debit', 'utils'],
-            favicon: './src/favicon.png',
-        }),
-    ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: {
+            utils: './src/utils.ts',
+            main: './src/index.ts',
+            debit: './src/debit.ts',
         },
-        compress: true,
-        port: 8787,
-        open: true,
-    },
-    mode: 'development'
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader'
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+                chunks: ['main', 'utils'],
+                favicon: './src/favicon.png',
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/debit.html',
+                filename: 'debit.html',
+                chunks: ['debit', 'utils'],
+                favicon: './src/favicon.png',
+            }),
+        ].concat(isProduction ? [new MiniCssExtractPlugin()] : []),
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            compress: true,
+            port: 8787,
+            open: true,
+        },
+        mode: isProduction ? 'production' : 'development',
+    };
 };
